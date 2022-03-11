49bcc9bc5bfa629bbd7a53e65f29b0ccab0cf63c
T <thaonpfx15559@funix.edu.vn>
2022-03-12 00:55:13 +0700

    solve problems in render department + false condition

--- src/components/main/ShowStaffInfo.js
+++ src/components/main/ShowStaffInfo.js
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import "./ShowStaffInfo.css";
-import { STAFFS , DEPARTMENTS } from "../../staffs.js";
+import { STAFFS } from "../../staffs.js";
+import dateFormat from "dateformat";
 
 export default function ShowStaffInfo() {
-  const [info, setInfo] = useState("bấm vào tên nhân viên để xem thông tin");
+  const [info, setInfo] = useState(null);
 
   const changeStaffInfo = (item) => {
     setInfo(item);
@@ -13,22 +14,26 @@ export default function ShowStaffInfo() {
     <div className="container">
       {STAFFS &&
         STAFFS.map((item) => (
-          <div key={item.id} className="item" onClick={() => changeStaffInfo(item)}>
+          <div
+            key={item.id}
+            className="item"
+            onClick={() => changeStaffInfo(item)}
+          >
             {item.name}
           </div>
         ))}
-
       {info ? (
         <ul className="item">
           <li>Họ và tên : {info.name}</li>
-          <li>Ngày sinh : {info.doB}</li>
-          <li>Ngày vào công ty : {info.startDate}</li>
-          {/* <li>Phòng ban : {info.department}</li> */}
+          <li>Ngày sinh : {dateFormat(info.doB, "dd/mm/yyyy")}</li>
+          <li>Ngày vào công ty : {dateFormat(info.startDate, "dd/mm/yyyy")}</li>
+          <li>Phòng ban : {info.department.name}</li>
           <li>Số ngày nghỉ còn lại : {info.annualLeave}</li>
           <li>Số ngày đã làm thêm : {info.overTime}</li>
         </ul>
-      ) : "bấm vào tên nhân viên để xem thông tin"}
+      ) : (
+        "bấm vào tên nhân viên để xem thông tin"
+      )}
     </div>
   );
 }
-
