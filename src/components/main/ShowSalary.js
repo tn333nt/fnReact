eba193ee9a0f91b0b4587fbee81422dbc05ac8e7
T <thaonpfx15559@funix.edu.vn>
2022-03-12 21:45:02 +0700

    update salary page

--- src/components/main/ShowSalary.js
+++ src/components/main/ShowSalary.js
@@ -1,3 +1,36 @@
 
 
 import "./ShowSalary.module.css" 
+import { STAFFS } from "../../staffs.js";
+
+export default function ShowStaffsInfo() {
+
+    const calculateSalary = (item) => {
+        const salaryScale = item.salaryScale
+        const overTime = item.overTime
+        const totalSalary = parseFloat(salaryScale*3000000) + parseFloat(overTime*((200000*3)/24))
+
+        return {
+            totalSalary
+        }
+    }
+
+  return (
+    <div className="container">
+      {STAFFS.map(item => (
+          <ul
+            key={item.id}
+            className="item"
+          >
+            <li>{item.name}</li>
+            <li>Mã nhân viên : {item.id}</li>
+            <li>Hệ số lương : {item.salaryScale}</li>
+            <li>Số giờ làm thêm : {item.overTime}</li>
+            <li>{calculateSalary(item)}</li>
+          </ul>
+        ))}
+      
+      <></>
+    </div>
+  );
+}
