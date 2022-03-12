13318dda083d2747b455da00ef5d545d7f467d44
T <thaonpfx15559@funix.edu.vn>
2022-03-12 22:27:30 +0700

    update ShowStaffsInfos

--- src/components/main/ShowStaffsInfo.js
+++ src/components/main/ShowStaffsInfo.js
@@ -1,8 +1,8 @@
-
-import "./ShowStaffsInfo.module.css";
+import { Routes, Route, Link } from "react-router-dom";
 import { useState } from "react";
 import { STAFFS } from "../../staffs.js";
-// import dateFormat from "dateformat";
+import ShowStaffDetails from "./ShowStaffDetails";
+import "./ShowStaffsInfo.module.css";
 
 export default function ShowStaffsInfo() {
   const [info, setInfo] = useState(null);
@@ -12,20 +12,26 @@ export default function ShowStaffsInfo() {
   };
 
   return (
-    <div className="container">
-      {STAFFS &&
-        STAFFS.map((item) => (
-          <div
-            key={item.id}
-            className="item"
-            onClick={() => changeStaffsInfo(item)}
-          >
-            {item.image}
-            {item.name}
-          </div>
-        ))}
-      
-      <></>
-    </div>
+    <Routes>
+      <Route className="container">
+        {STAFFS &&
+          STAFFS.map((item) => (
+            <Link
+              key={item.id}
+              className="item"
+              onClick={() => changeStaffsInfo(item)}
+              to={`/${info.id}`} // set link for each item
+            >
+              <img src="{item.image}" alt="" />
+              <p>{item.name}</p>
+            </Link>
+          ))}
+      </Route>
+
+      <Route path={`/${info.id}`} element={<ShowStaffDetails />} /> 
+      {/* link link to content */}
+    </Routes>
   );
 }
+
+// sai sao sao:D
