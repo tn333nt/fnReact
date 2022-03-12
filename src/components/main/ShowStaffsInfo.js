7d47cf2118161fb69ba750c57283899c03084e2b
T <thaonpfx15559@funix.edu.vn>
2022-03-12 23:42:32 +0700

    update useNavigate()

--- src/components/main/ShowStaffsInfo.js
+++ src/components/main/ShowStaffsInfo.js
@@ -1,37 +1,31 @@
-import { Routes, Route, Link } from "react-router-dom";
-import { useState } from "react";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import { STAFFS } from "../../staffs.js";
 import ShowStaffDetails from "./ShowStaffDetails";
 import "./ShowStaffsInfo.module.css";
 
 export default function ShowStaffsInfo() {
-  const [info, setInfo] = useState(null);
 
-  const changeStaffsInfo = (item) => {
-    setInfo(item);
-  };
+  const navigate = useNavigate()
 
   return (
     <Routes>
       <Route className="container">
         {STAFFS &&
           STAFFS.map((item) => (
-            <Link
+            <div
               key={item.id}
               className="item"
-              onClick={() => changeStaffsInfo(item)}
-              to={`/${info.id}`} // set link for each item
+              onClick={() => navigate(item)}
             >
-              <img src="{item.image}" alt="" />
+              <Link to={`/staffs/${STAFFS.id}`} >
+                < img src="{item.image}" alt="" />
+              </Link>
               <p>{item.name}</p>
-            </Link>
+            </div>
           ))}
       </Route>
 
-      <Route path={`/${info.id}`} element={<ShowStaffDetails />} /> 
-      {/* link link to content */}
+      <Route path={`/staffs/${STAFFS.id}`} element={<ShowStaffDetails />} /> 
     </Routes>
   );
 }
-
-// sai sao sao:D
