30d0e35fe38f782b38e295f3ed721fa88087da04
T <thaonpfx15559@funix.edu.vn>
2022-03-13 23:48:17 +0700

    import staff as route

--- src/components/main/ShowStaffsInfo.js
+++ src/components/main/ShowStaffsInfo.js
@@ -1,27 +1,40 @@
-import { Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { useState } from "react";
 import { STAFFS } from "../../staffs.js";
+import ShowStaffDetails from "./ShowStaffDetails";
 import "./ShowStaffsInfo.module.css";
 
 export default function ShowStaffsInfo() {
-  const [info, setInfo] = useState();
+  const [info, setInfo] = useState([]);
+
   const handleInfo = (item) => {
     setInfo(item);
   };
 
   return (
-    <div className="container">
-      <h1>Nhân viên</h1>
-      <hr />
-      {STAFFS &&
-        STAFFS.map((item) => (
-          <div key={item.id} className="item">
-            <Link to={`/staffs/${info.id}`}>
-              <img src="{item.image}" alt="" onClick={() => handleInfo(item)} />
-            </Link>
-            <p>{item.name}</p>
-          </div>
-        ))}
-    </div>
+    <Routes>
+      <Route className="container">
+        <h1>Nhân viên</h1>
+        <hr />
+        {STAFFS &&
+          STAFFS.map((item) => (
+            <div key={item.id} className="item">
+              <Link to={`/staffs/${info.id}`}>
+                <img
+                  src="{item.image}"
+                  alt=""
+                  onClick={() => handleInfo(item)}
+                />
+              </Link>
+              <p>{item.name}</p>
+            </div>
+          ))}
+      </Route>
+
+      <Route
+        path={`/staffs/${info.id}`}
+        element={<ShowStaffDetails info={info} />}
+      />
+    </Routes>
   );
 }
