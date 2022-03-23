0d43f828c1f4c4f7211aaa78ba0c9a5a45333fcd
thaonp <thaonpfx15559@funix.edu.vn>
2022-03-24 00:13:06 +0700

    test

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import { Provider } from "react-redux"
-import store from "./redux/store"
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
+      {/* test */}
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
